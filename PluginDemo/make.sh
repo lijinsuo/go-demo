650f14b766a3a79881a0a2aed962dc2d54e0ece4
singwhatiwanna <singwhatiwanna@163.com>
2017-06-12 12:13:24 +0800

    add flavors to plugin demo

--- PluginDemo/make.sh
+++ PluginDemo/make.sh
@@ -1,4 +1,4 @@
-gradle clean aP
-adb push app/build/outputs/apk/app-release-unsigned.apk /sdcard/Test.apk
+gradle clean assemblePlugin
+adb push app/build/outputs/apk/app-beijing-release-unsigned.apk /sdcard/Test.apk
 adb shell am force-stop com.didi.virtualapk
 adb shell am start -n com.didi.virtualapk/com.didi.virtualapk.MainActivity
